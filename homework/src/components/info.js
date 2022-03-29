0beaf0bb5671d618d6f26d3d4f04d464ecddc78f
Excelsiooor! <homenich101@gmail.com>
2022-03-29 22:03:07 +0300

    some change in first use

---
+++ homework/src/components/info.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>weather-web</h2>
+                <p>view weather in city!</p>
+            </div>
+        );
+    }
+}
+
+export default Info;
