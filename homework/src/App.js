eca601ee1d99ee3bef6c42982ca0ae016856c0f0
Excelsiooor! <homenich101@gmail.com>
2022-03-31 19:49:26 +0300

    add api and some components

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
+
+const API_KEY = "05f3333daf7e19734aa5b39160cd7357";
 
 class App extends React.Component {
+
+  gettingWeather = async () => {
+    const api_url = await
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=Dnipro_ua&appid=${API_KEY}&units=metric`);
+    const data = await api_url.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div>
         <Info />
+        <Form />
+        <Weather />
       </div>
     );
   }
---
+++ homework/src/components/form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Form extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>Component Form</div>
+            </div>
+        );
+    }
+}
+
+export default Form;
